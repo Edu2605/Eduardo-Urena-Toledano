fc25048f65a83c4b89a27ab1b92c08845409560d
Edu2605 <47172288+Edu2605@users.noreply.github.com>
2019-04-11 13:46:28 +0200

    Add files via upload

---
+++ microp_7/src/app/firebase.credentials.ts
@@ -0,0 +1,10 @@
+
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyA6J42XltiYlzNEl_vWr0lkt5qxnrcW-YE",
+    authDomain: "microp-7.firebaseapp.com",
+    databaseURL: "https://microp-7.firebaseio.com",
+    projectId: "microp-7",
+    storageBucket: "microp-7.appspot.com",
+    messagingSenderId: "662119574099"
+    };
+   
---
+++ microp_7/src/services/contact.service.ts
@@ -0,0 +1,31 @@
+import { Contact } from "../models/contact.model";
+import { Injectable } from "@angular/core";
+import { AngularFireDatabase } from "angularfire2/database";
+
+Injectable()
+export class ContactService{
+ //private contacts: Contact []=
+//[{"nombre":"Andres","organizacion":"UC3M","movil":"666666666",correo:"andres@example.com"}];
+
+    private contactsRef=this.db.list<Contact>('AgendaFirebase');
+    constructor(private db:AngularFireDatabase){
+    }
+
+    addContact(value: Contact){
+        //this.contacts.push(value);
+        return this.contactsRef.push(value);
+    }
+
+    getContacts(){
+        //return this.contacts;
+        return this.contactsRef;
+    }
+
+    updateContact(value: Contact){
+        return this.contactsRef.update(value.key,value);
+    }
+
+    removeContact(value: Contact){
+        return this.contactsRef.remove(value.key);
+    }
+}
